c0c5863be910e3f65d1a7a1113a5f22f58a35a22
Arham Jain <9jainarham12345@gmail.com>
2024-06-28 18:30:17 +0530

    Made upto registeration form

--- mydb.py
+++ mydb.py
@@ -6,8 +6,11 @@ database=mysql.connector.connect(
     passwd='Jain@321'
 )
 
+#A cursor allows you to execute SQL queries, fetch data from the database, and manage the context of a fetch operation.
 cursorObject=database.cursor()
 
+#cursorObject.execute() is a method used to execute a single SQL statement. In this case, it executes the SQL command to create a new database named elderco.
+#The cursor object communicates with the MySQL server and sends the command for execution.
 cursorObject.execute("CREATE DATABASE elderco")
 
 print("ALL DONE")
--- website/views.py
+++ website/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
+from .forms import SignUpForm
 
-# Create your views here.
+# request is going to that webpage and requesting that webpage
+def home(request):
+    #Check to see if logging in
+
+    if request.method=='POST': #This checks if the HTTP request method is POST. Typically, a POST request is used for form submissions (like logging in).
+        username=request.POST['username'] #Whataver user mentiones in the username field in form it will be stored in the variable username
+        password=request.POST['password'] #Same above like of username for password
+        #Authenicate checks credentials provided returning usr object if correct else None
+        user=authenticate(request,username=username,password=password)
+
+        if user is not None:
+
+#If authenticate returns a valid user (user is not None), the login function logs the user in by attaching the user object to the session.
+#messages.success is used to set a success message to be displayed to the user.
+
+            login(request,user)
+            messages.success(request,"You have been logged in")
+            return redirect('home') #Where we have authentication true 
+    
+        else: #Handleing None condition
+            messages.success(request,"There was an error logging in please try again....") #else part in home html where we have the same webpag with error thrown above.
+            return redirect('home')
+    else: #Handle GET Condition
+        return render(request,'home.html',{})
+    
+
+def logout_user(request):
+    logout(request)
+    messages.success(request,"You have been logged out !")
+    return render(request,'home.html',{})
+
+def register_user(request):
+    if request.method=='POST':
+        form=SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            #Authenticate and login
+            username=form.cleaned_data['username']
+            password=form.cleaned_data['password1']
+            user=authenticate(username=username,password='password')
+            login(request,user)
+            messages.success(request,"You have succesfully registered.")
+            return redirect('home')
+    else:
+        form=SignUpForm()    
+    return render(request,'register.html',{'form':form})
